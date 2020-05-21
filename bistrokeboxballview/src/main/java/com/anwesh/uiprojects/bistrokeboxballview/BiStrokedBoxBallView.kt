c623fe8ae08a8a8b97be92a7c05b7231fa4f82af
Anwesh43 <anweshthecool0@gmail.com>
2020-05-22 05:11:00 +0530

    adding BiStrokedBoxBallView class

--- bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
+++ bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
     drawStrokedBoxBall(scale, w, size, paint)
     restore()
 }
+
+class BiStrokedBoxBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
