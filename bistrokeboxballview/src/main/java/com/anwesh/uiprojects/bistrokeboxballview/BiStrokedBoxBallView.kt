b461afaefdcf7dfca6b0f99ce767ac7790300577
Anwesh43 <anweshthecool0@gmail.com>
2020-05-22 04:58:32 +0530

    adding constants and libraries

--- bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
+++ bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bistrokeboxballview
 /**
  * Created by anweshmishra on 22/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
