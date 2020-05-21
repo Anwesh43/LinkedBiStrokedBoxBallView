db746a2a7fc06697763b9eb1a6557a9197eeea1f
Anwesh43 <anweshthecool0@gmail.com>
2020-05-22 05:23:04 +0530

    adding BiStrokeBoxBall class

--- bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
+++ bistrokeboxballview/src/main/java/com/anwesh/uiprojects/bistrokeboxballview/BiStrokedBoxBallView.kt
@@ -167,4 +167,28 @@ class BiStrokedBoxBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiStrokeBoxBall(var i : Int) {
+
+        private val root : BSBNode = BSBNode(0)
+        private var curr : BSBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
